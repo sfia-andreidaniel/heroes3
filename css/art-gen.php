af428dae928e16fbb9b3508d0590acdad0a08739
sfia-andreidaniel <sfia.andreidaniel@gmail.com>
2014-05-27 11:13:32 +0300

    working on creatures

--- css/art-gen.php
+++ css/art-gen.php
@@ -1,13 +1,13 @@
 <?php
     
-    $index = ( 3 * 56 );
+    $index = ( 3 * 57 );
     
-    for ( $row = 0; $row < 56; $row++ ) {
+    for ( $row = 0; $row < 57; $row++ ) {
     
         for ( $col = 2; $col >= 0; $col-- ) {
             
             
-            echo "div.g-art.id-$index: {\n    background-position: " , ( -$col * 64 ) , "px ", ( -$row * 64 ), "px;\n}\n\n";
+            echo "div.g-mon.id-$index: {\n    background-position: " , ( -$col * 64 ) , "px ", ( -$row * 64 ), "px;\n}\n\n";
             $index--;
         }
         
